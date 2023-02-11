332f1e2ac0f49241084da942649b168997724ba0
Nikulenko Denys <denis.n@redcat.dev>
2023-02-11 15:00:50 +0200

    set up eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    extends: ['plugin:react/recommended', 'plugin:prettier/recommended', '@react-native-community'],
+    plugins: ['react', 'react-hooks'],
+    rules: {
+        'react-hooks/exhaustive-deps': 'off',
+        'no-unused-vars': 2,
+        'prettier/prettier': 0,
+    },
 };
